2a869e976a055240db4437dd82e7ad291633e6cc
Timo Toots <info@timo.ee>
2022-06-11 15:44:49 +0300

    hack

---
+++ software/videoplayer/functions.js
@@ -0,0 +1,30 @@
+function sendCmdDisplayServer(getParams){
+
+    getJSON("http://localhost:3000", getParams)
+    .then(data => {
+        console.log(data);
+    });
+    
+}
+
+function getJSON(url, qs_params) {
+    function buildQueryString(params) {
+      return Object.entries(params).map(d => `${d[0]}=${d[1]}`).join('&');
+    }
+
+    return new Promise((resolve, reject) => {
+      const qs = qs_params ? '?' + buildQueryString(qs_params) : '';
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `${url}${qs}`);
+
+      xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 400) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          resolve(xhr.responseText);
+        }
+      };
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+  }
