f65a4818c2f0aa0697e7fb49c3a381f913c351cc
Timo Toots <info@timo.ee>
2022-07-16 22:11:38 +0300

    files

--- software/gui/music/files.php
+++ software/gui/music/files.php
@@ -3,29 +3,38 @@
 // add to rc.local
 // php -S 0.0.0.0:8000 -t /opt/videopiano/software/gui/
 
-$files = glob('neothesia/*.mp4');
-
 $out = [];
-foreach ($files as $filename) {
 
-	$o = [];
-	$o["url"] = "http://videopiano1.local:8000/music/" . $filename; 
-	
-  $o["track_name"] = $filename;
-	$o["track_name"] = str_replace("neothesia/", "", $o["track_name"]);
-	$o["track_name"] = str_replace(".mid.mp4", "", $o["track_name"]);
-	$o["track_name"] = str_replace("_", " ", $o["track_name"]);
+function getDir($dir, $video_height, $mapping_piano_top_y){
 
-  $o["type"] = "video/mp4";
-  $o["composer"] = "";
-  $o["credits"] = "";
-  $o["video_height"] = 360;
-  $o["mapping_piano_top_y"] = 245;
-  $o["first_note_time"] = 0;
+  global $out;
 
-  $out[] = $o;
+  $files = glob($dir.'/*.mp4');
+
+  foreach ($files as $filename) {
+
+    $o = [];
+    $o["url"] = "http://videopiano1.local:8000/music/" . $filename; 
+    
+    $o["track_name"] = $filename;
+    $o["track_name"] = str_replace($dir, "", $o["track_name"]);
+    $o["track_name"] = str_replace(".mid.mp4", "", $o["track_name"]);
+    $o["track_name"] = str_replace("_", " ", $o["track_name"]);
+  
+    $o["type"] = "video/mp4";
+    $o["composer"] = "";
+    $o["credits"] = "";
+    $o["video_height"] = 360;
+    $o["mapping_piano_top_y"] = 245;
+    $o["first_note_time"] = 0;
+  
+    $out[] = $o;
+  
+  }
 
 }
+
+getDir("neothesia/dest",360,245);
 
 print json_encode($out, JSON_PRETTY_PRINT);
 
